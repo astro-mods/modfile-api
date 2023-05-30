2dd1486164a2f7e84d35831b12efd9485a04d447
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-05-30 13:28:21 -0400

    add rate limits

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const uploadRouter = require('./upload');
 const contentRouter = require('./content');
-
-
 
 const app = express();
 
 // Enable CORS
 app.use(cors());
 
+// Rate limiting configuration
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // Limit each IP to 10 requests per minute
+});
+
+// Apply the rate limiter to all requests
+app.use(limiter);
+
 // Mount the file upload router
 app.use('/upload', uploadRouter);
-app.use ('/content', contentRouter);
+app.use('/content', contentRouter);
 
 app.get('/healthz', (req, res) => {
   res.send('OK');
 });
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
